2544859fd61f9a758e29f635d8ec8bdb843a075a
DevPedroHB <pedrohenriquebergamo.16@gmail.com>
2020-06-21 00:26:28 -0300

    Feito o Update

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -5,5 +5,6 @@ const UsuarioController = require('../controllers/usuarioController.js');
 router.post('/usuarios', UsuarioController.Insert);
 router.get('/usuarios', UsuarioController.SearchAll);
 router.get('/usuarios/:id', UsuarioController.SearchOne);
+router.put('/usuarios/:id', UsuarioController.Update);
 
 module.exports = router;
