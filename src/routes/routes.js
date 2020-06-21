07d07e8b32ae51204acf6eb5e2133c975c42f919
DevPedroHB <pedrohenriquebergamo.16@gmail.com>
2020-06-21 00:01:34 -0300

    Feito o Select

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -3,5 +3,7 @@ const router = express.Router();
 const UsuarioController = require('../controllers/usuarioController.js');
 
 router.post('/usuarios', UsuarioController.Insert);
+router.get('/usuarios', UsuarioController.SearchAll);
+router.get('/usuarios/:id', UsuarioController.SearchOne);
 
 module.exports = router;
